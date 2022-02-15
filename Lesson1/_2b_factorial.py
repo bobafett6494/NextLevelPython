b05b64685f797ed9c8573cad43a054f2f543a2bf
bobafett6494 <stephan.knoell@gmail.com>
2022-02-15 22:30:27 +0100

    Factorials done with Recursion

--- Lesson1/_2b_factorial.py
+++ Lesson1/_2b_factorial.py
@@ -15,11 +15,26 @@ def equals(actual, expected):
 
 # Todo: Create a function that produces the factorial of a number
 
+def factorial(number):
+    if number <= 1:
+        return 1
+    else:
+        return number * factorial(number-1)
 
 # Todo: Test factorial function
 
+equals(factorial(3), 6)
+equals(factorial(2), 2)
+equals(factorial(4), 24)
 
 # Todo: Request a number from the user
 
-
+number=int(input("Type in a number: "))
 # Todo: Print a list of factorials from 0 to the given number
+
+list_factorials=[]
+
+for n in range(number):
+    list_factorials.append(factorial(n))
+
+print(f"Factorial list: {list_factorials}")
