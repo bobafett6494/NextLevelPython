c1fba39e584bc1e28d145a616029aa527055c7f8
bobafett6494 <stephan.knoell@gmail.com>
2022-02-16 11:56:55 +0100

    1c countries solved

--- Lesson2/_1c_countries.py
+++ Lesson2/_1c_countries.py
@@ -4,11 +4,24 @@ all of the countries that start with that letter
 """
 
 # Todo: Read data/countries.txt and save all countries
+with open("data/countries.txt", "r") as file:
+    list_countries = []
+    for line in file.readlines():
+        list_countries.append(line)
 
 # Get user to provide a letter
 letter = input('Number of countries that start with letter: ')
 letter = letter.capitalize()
 
 # Todo: Print the number of countries that start with the letter
+#count = 0
+list_countries_startwithletter = []
+for country in list_countries:
+    if country.startswith(letter): #Or country[0].upper() == letter
+        #count +=1
+        list_countries_startwithletter.append(country)
+print(f"Number of countries starting with chosen letter '{letter}': {len(list_countries_startwithletter)}")
 
 # Todo: Print the countries that start with the letter
+
+print(f"List of countries starting with chosen letter: {list_countries_startwithletter}")
