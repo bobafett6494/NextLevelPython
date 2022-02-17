187929efb791f92d68ae84ef7dcd9a08ff17a308
bobafett6494 <stephan.knoell@gmail.com>
2022-02-17 19:10:13 +0100

    Solved 3c

--- Lesson2/_3c_countries.py
+++ Lesson2/_3c_countries.py
@@ -7,13 +7,21 @@ all of the countries on that continent
 import csv
 
 with open("data/continents.csv", "r") as file:
-     reader = csv.DictReader(file)
+    reader = csv.DictReader(file)
 
-print(reader)
+    continent_countries={}
 
-continent_countries={}
-for line in reader:
-    continent_countries['XX'] = 'New capital'
+    for line in reader:
+        #print(line["Continent"])
+        list_countries = []
+        if line["Continent"] not in continent_countries and line["Continent"] != "Continent":
+            list_countries.append(line["Country"])
+            continent_countries[line["Continent"]] = list_countries
+
+        else:
+            continent_countries[line["Continent"]].append(line["Country"])
+
+    print(continent_countries)
 
 continents = [
     'Africa',
@@ -25,9 +33,11 @@ continents = [
 ]
 
 # Todo: Get user to provide a continent by inputting a number
-print('Choose a continent:')
+number_chosen = int(input('Choose a continent:'))
 print('1: Africa, 2: Asia, 3: Europe, 4: North America, 5: Oceania, 6: South America')
 
 # Todo: Print the number of countries on that continent
+print(len(continent_countries.get(continents[number_chosen-1])))
 
 # Todo: Print each of the countries on that continent to the console
+print(continent_countries.get(continents[number_chosen-1]))
